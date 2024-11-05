84a7a4c8c4a310bf4cb03e795a40d4f1168e7b3c
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-11-05 20:36:46 -0300

    refactor(toggle-theme.js): removing all comments unnecessary of scope from code

--- public/scripts/toggle-theme.js
+++ public/scripts/toggle-theme.js
@@ -1,35 +1,9 @@
-// const toggleThemeBtn = document.getElementById('toggle-theme')
-// let currentTheme = 'light' // Pode ser inicializado com o tema padrão
-
-// toggleThemeBtn.addEventListener('click', () => {
-//   if (currentTheme === 'light') {
-//     document.body.classList.remove('light-theme')
-//     document.body.classList.add('dark-theme')
-//     toggleThemeBtn.classList.replace('bi-moon-fill', 'bi-brightness-high-fill')
-//     currentTheme = 'dark'
-//   } else if (currentTheme === 'dark') {
-//     document.body.classList.remove('dark-theme')
-//     document.body.classList.add('custom-theme')
-//     toggleThemeBtn.classList.replace(
-//       'bi-brightness-high-fill',
-//       'bi-palette-fill'
-//     )
-//     currentTheme = 'custom'
-//   } else {
-//     document.body.classList.remove('custom-theme')
-//     document.body.classList.add('light-theme')
-//     toggleThemeBtn.classList.replace('bi-palette-fill', 'bi-moon-fill')
-//     currentTheme = 'light'
-//   }
-// })
-
 const toggleThemeBtn = document.getElementById('toggle-theme')
 const lightThemeBtn = document.getElementById('light-theme')
 const customThemeBtn = document.getElementById('custom-theme')
 const systemThemeBtn = document.getElementById('system-theme')
-let currentTheme = 'light' // Inicializa com o tema padrão
+let currentTheme = 'light'
 
-// Função para aplicar o tema de acordo com o sistema operacional
 function applySystemTheme() {
   const prefersDarkScheme = window.matchMedia(
     '(prefers-color-scheme: dark)'
@@ -45,7 +19,6 @@ function applySystemTheme() {
   }
 }
 
-// Eventos de clique para alternar entre os temas
 toggleThemeBtn.addEventListener('click', () => {
   document.body.classList.remove('light-theme', 'custom-theme')
   document.body.classList.add('dark-theme')
@@ -69,7 +42,6 @@ systemThemeBtn.addEventListener('click', () => {
   currentTheme = 'system'
 })
 
-// Aplica o tema do sistema automaticamente na carga da página se o botão de tema do sistema for padrão
 window.addEventListener('load', () => {
   if (currentTheme === 'system') {
     applySystemTheme()
