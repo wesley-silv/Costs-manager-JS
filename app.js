14bd24ab1d761070e744b226d08c9dff4c20a982
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-11-08 20:30:32 -0300

    fix(app.js): config path

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(
 app.use(express.urlencoded({ extended: true }))
 
 // Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'src')))
+app.use(express.static(path.join(__dirname, 'public' || __dirname, 'src')))
 
 // Função de middleware para verificar autenticação
 function requireAuth(req, res, next) {
